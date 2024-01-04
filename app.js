8a61c780f0eff74e5a275560ab20e5d328e585ee
Khan Kamran Aslam <153853096+KhanKamranAslam@users.noreply.github.com>
2024-01-04 13:31:22 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generateQR() {
+  if (qrText.value.length > 0) {
+
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+      qrText.style.borderColor = "green";
+    imgBox.classList.add("show-img");
+  }else{
+    qrText.classList.add("error");
+    setTimeout(() => {
+        qrText.style.borderColor = "red";
+        qrText.classList.remove("error");
+    },1000);
+  }
+}
